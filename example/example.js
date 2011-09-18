64935174fe47463b52f7f020503b6678ae95ce20
Pavel Minchenkov <chardex@gmail.com>
2011-09-19 00:32:03 +0400

    Error object as first callback argument

--- example/example.js
+++ example/example.js
@@ -1,17 +1,12 @@
 var SoapClient = require('../lib/node-soap-client.js').SoapClient;
 
-new SoapClient({
-	wsdl: 'http://api.metabus.ru/0.0.1/ws/SearchingModule?WSDL',
-	success: function(metabus) {
-		var searchingModule = new metabus.SearchingModule();
+new SoapClient({wsdl: 'http://api.metabus.ru/0.0.1/ws/SearchingModule?WSDL'}, function(err, metabus) {
+    var searchingModule = new metabus.SearchingModule();
 
-		searchingModule.search(new metabus.SearchQuery({geoFilter: {distance: 10}, text: 'кофе около кремля'}), function(result) {
-			console.log(result)
-		}, function(fault){
-			console.log(fault.children[1].text())
-		});
-	},
-	error: function(err) {
-		throw err;
-	}
+    searchingModule.search({geoFilter: {distance: 10}, text: ''}, function(err, result) {
+        if (err)
+            console.log(err.children[1].text())
+        else
+            console.log(result);
+    });
 });
